c9e64909397bad1e082fcf0e0c09a71f5d3efc6f
Roman <s91630277@gmail.com>
2015-02-14 00:43:34 +0300

    Removed getFakeObject.js

--- sandbox/ex1.js
+++
@@ -1,8 +0,0 @@
-var read = require("./read2");
-var Stringify = require('streaming-json-stringify');
-var JSONStream = require("JSONStream");
-
-read
-	// .pipe(Stringify())
-	.pipe(JSONStream.stringify())
-	.pipe(process.stdout)
